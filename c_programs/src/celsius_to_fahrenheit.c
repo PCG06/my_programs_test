fdc90668d2020553224b3f27868dd4fe34f74d2b
PCG06 <pcggaming06@gmail.com>
2024-07-21 15:53:22 +0530

    Celcius to Fahrenheit and vice versa

---
+++ c_programs/src/celsius_to_fahrenheit.c
@@ -0,0 +1,17 @@
+// C program to convert Celsius to Fahrenheit
+
+#include <stdio.h>
+
+int main()
+{
+    float cels, fahr;
+
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &cels);
+
+    fahr = (cels * 9/5) + 32;
+
+    printf("\nTemperature %.2f in Celsius is %.2f in Fahrenheit\n", cels, fahr);
+
+    return 0;
+}
---
+++ c_programs/src/fahrenheit_to_celsius.c
@@ -0,0 +1,17 @@
+// C program to convert Fahrenheit to Celsius 
+
+#include <stdio.h>
+
+int main()
+{
+    float fahr, cels;
+
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &fahr);
+
+    cels = (fahr - 32) * 5/9;
+
+    printf("\nTemperature %.2f in Fahrenheit is %.2f in Celsius\n", fahr, cels);
+
+    return 0;
+}
