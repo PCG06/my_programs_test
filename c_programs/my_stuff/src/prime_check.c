c2176d478c11981d8a95dc84a789c3ebd5e67ae4
PCG06 <pcggaming06@gmail.com>
2024-07-24 19:59:09 +0530

    Update prime numbers program

--- c_programs/my_stuff/src/prime_check.c
+++ c_programs/my_stuff/src/prime_check.c
@@ -1,35 +1,17 @@
 // C program to check whether number read is a prime number or not 
 
 #include <stdio.h>
-#include <stdbool.h>
-
-// Declaring the function before defining it let's me call the function before it's definition.
-static bool isPrimeNumber(int num);
 
 void main()
 {
-    int num;
+    unsigned long int num, i, primeNum = 1;
 
     printf("Enter a number: ");
-    scanf("%d", &num);
-
-    if (isPrimeNumber(num) == true)
-    {
-        printf("\n%d is a prime number!\n", num);
-    }
-    else
-    {
-        printf("\n%d is not a prime number!\n", num);
-    }
-}
-
-static bool isPrimeNumber(int num)
-{
-    int i;
+    scanf("%lu", &num);
 
     if (num <= 1)
     {
-        return false;
+        primeNum = 0;
     }
     else
     {
@@ -37,11 +19,18 @@ static bool isPrimeNumber(int num)
         {
             if (num % i == 0)
             {
-                return false;
+                primeNum = 0;
+                break;
             }
         }
-        return true;
     }
 
-    return false;
+    if (primeNum)
+    {
+        printf("%lu is a prime number.\n", num);
+    }
+    else
+    {
+        printf("%lu is not a prime number.\n", num);
+    }
 }
