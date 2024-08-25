4ca4e0f82a54f57bdd345a6ddef944a96496e1c0
PCG06 <pcggaming06@gmail.com>
2024-08-25 16:41:32 +0530

    Program to print a random 16-bit number

---
+++ src/random.c
@@ -0,0 +1,24 @@
+// C program to print a random 16-bit number
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int Random()
+{
+    // Generate a random number and mask it to fit within 16 bits
+    return (int)(rand() & 0xFFFF);
+}
+
+void main()
+{
+    srand((unsigned int)time(NULL));
+    unsigned int num, random;
+
+    printf("Enter maximum limit: ");
+    scanf("%u", &num);
+
+    random = (Random() % num) + 1;
+
+    printf("Random 16-bit number: %u\n", random);
+}
