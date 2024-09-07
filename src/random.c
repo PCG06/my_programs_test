80e9626664fa332bb08737310783088e2b94aca6
PCG06 <pcggaming06@gmail.com>
2024-09-07 09:57:09 +0530

    Edits to the random function

--- src/random.c
+++ src/random.c
@@ -6,19 +6,19 @@
 
 int Random()
 {
+    srand((unsigned int)time(NULL));
     // Generate a random number and mask it to fit within 16 bits
     return (int)(rand() & 0xFFFF);
 }
 
 void main()
 {
-    srand((unsigned int)time(NULL));
     unsigned int num, random;
 
     printf("Enter maximum limit: ");
     scanf("%u", &num);
 
-    random = (Random() % num) + 1;
+    random = (Random() % num + 1);
 
     printf("Random 16-bit number: %u\n", random);
 }
