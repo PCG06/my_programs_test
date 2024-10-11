49923be349090427c0a2b0a936d13b5598e94258
PCG06 <pcggaming06@gmail.com>
2024-10-11 16:31:38 +0530

    Program to check whether a year is a leap year or not

---
+++ src/leap_year.c
@@ -0,0 +1,18 @@
+// C program to check whether a year is a leap year or not
+
+#include <stdio.h>
+
+void main()
+{
+    int year = 2000, isLeap = 0;
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        isLeap = 1;
+
+    if (isLeap)
+        printf("It is a leap year\n");
+    else
+        printf("It is not a leap year\n");
+}
