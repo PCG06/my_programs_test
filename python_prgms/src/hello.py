ffce1b478ffa151e4d6d6404f7ee828deccfd336
PCG06 <pcggaming06@gmail.com>
2024-11-20 19:16:07 +0530

    Introduction to Python Also added a CI build for it. Hope it won't fail on first try!

---
+++ python_prgms/src/hello.py
@@ -0,0 +1,13 @@
+my_name = "PCG"
+my_age = 18
+my_hobbies = ["binging TV shows", "reading history", "programming", "playing shooter games"]
+
+if len(my_hobbies) > 1:
+    my_hobbies.sort(key=str.lower)
+    hobbies_str = ", ".join(my_hobbies[:-1]) + " and " + my_hobbies[-1] + "."
+else:
+    hobbies_str = my_hobbies[0] + "."
+
+print("My name is", my_name)
+print("I am {} years old".format(my_age))
+print("My hobbies are", hobbies_str)
