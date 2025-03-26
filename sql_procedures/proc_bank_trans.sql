d721dd9a2101df505dcd2a0e788e2077202f70d9
PCG06 <pcggaming06@gmail.com>
2025-03-26 17:02:33 +0530

    INITCAP

--- sql_procedures/proc_bank_trans.sql
+++ sql_procedures/proc_bank_trans.sql
@@ -7,7 +7,7 @@ XYZ: begin
     declare finished int default 0;
     declare x int default 0;
     declare bal_amt double(10,2) default 0;
-    declare cur_bank cursor for select balance from bank_account where acc_no = account_no;
+    declare cur_bank cursor for select Balance from bank_account where Acc_No = account_no;
     declare continue handler for not found set finished = 1;
     open cur_bank;
     fetch cur_bank into bal_amt;
@@ -19,7 +19,7 @@ XYZ: begin
     if (bal_amt - amt) < 1000 then
         set trans_status = 'Maintain minimum balance';
     else
-        update bank_account set balance = bal_amt - amt where acc_no = account_no;
+        update bank_account set Balance = bal_amt - amt where Acc_No = account_no;
         select row_count() into x;
         if x = 1 then
             set trans_status = 'Transaction successful';
--- sql_procedures/proc_rating.sql
+++ sql_procedures/proc_rating.sql
@@ -8,7 +8,7 @@ begin
     declare c_name varchar(20);
     declare c_score int;
     declare c_risk varchar(15);
-    declare cur_rate cursor for select company_name, score, risk from company_credit_score;
+    declare cur_rate cursor for select Company_Name, Score, Risk from company_credit_score;
     declare continue handler for not found set finished = 1;
     open cur_rate;
     XYZ: loop
@@ -18,15 +18,15 @@ begin
         end if;
         case
             when c_score >= 30 then
-                set c_risk = 'Low risk';
+                set c_risk = 'Low Risk';
             when c_score >= 21 and c_score < 30 then
-                set c_risk = 'Medium risk';
+                set c_risk = 'Medium Risk';
             when c_score >= 11 and c_score < 20 then
-                set c_risk = 'High risk';
+                set c_risk = 'High Risk';
             else
                 set c_risk = 'Junk';
         end case;
-        update company_credit_score set risk = c_risk where company_name = c_name;
+        update company_credit_score set Risk = c_risk where Company_Name = c_name;
     end loop XYZ;
     close cur_rate;
 end $$
