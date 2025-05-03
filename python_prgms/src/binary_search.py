7b9c0944bfa13c79162d1d02b863f0ee16066a3d
PCG06 <pcggaming06@gmail.com>
2025-05-03 17:02:01 +0530

    Binary search in python Beginning to write in Python again at basic level with arrays.

---
+++ python_prgms/src/binary_search.py
@@ -0,0 +1,33 @@
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid + 1 # 1-based index
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+def main():
+    arr = sorted([int(i) for i in input("Enter numbers: ").split()]) # Sorts the input array
+    target = int(input("Enter number to search: "))
+
+    # Print array elements
+    print("The sorted array elements are:")
+    print(*arr) # No need to loop while printing array elements. Use * to avoid [sqaure brackets around numbers]
+
+    result = binary_search(arr, target)
+
+    if result != -1:
+        print(f"Found at position {result}")
+    else:
+        print("Not found")
+
+if __name__ == "__main__":
+    main()
